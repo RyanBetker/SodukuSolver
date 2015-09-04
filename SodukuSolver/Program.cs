adf05a73c74322861154937aba3bca357dd33d07
Ryan Betker <technologyRyan@outlook.com>
2015-09-03 20:56:20 -0500

    Bug to fix: Has inifinite loop on SolveSolution.

--- SodukuSolver/Program.cs
+++ SodukuSolver/Program.cs
@@ -44,9 +44,11 @@ namespace SodukuSolver
 
                     if (LegalMoveChecker.IsLegalMove(currentCell, sodukuGameData, cellValue))
                     {
+#if DEBUG
                         PrintGame(sodukuGameData);
                         Console.WriteLine("\n\n");
-
+                        
+#endif
                         currentCell.Value = cellValue;
                         isLegalMove = true;
                     }
